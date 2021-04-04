fbe18ae2cb8139d4cb62ac895be180459744ef42
7204-OMER <Eyupbhr24@gmail.com>
2021-04-04 20:23:12 +0300

    results and screen writing

--- app.js
+++ app.js
@@ -1,3 +1,18 @@
-function {
-    if(input === )
-}
+let btn = document.querySelector("#btn")
+let result = document.getElementById("result");
+let wanted = parseInt(100*Math.random());
+btn.addEventListener("click",find)
+function find(){
+    let num  = document.getElementById("num").value;
+    console.log(wanted);
+    console.log(result);
+    console.log(num)
+        if (parseInt(num) === wanted){
+            result.textContent = "Congrats!!";
+        }else if (parseInt(num) < wanted){
+            result.textContent = "Try Higher Numbers!!";
+        }else if (parseInt(num) > wanted){
+            result.textContent = "Try Lower Numbers!!";
+        }
+    // location.reload();
+    }
